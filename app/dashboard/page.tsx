2987d9bb95c913acdfff2754c615e000425cf444
Yannick Bonavoglia <yannick.bonavoglia13@gmail.com>
2024-01-24 02:29:46 +0100

    added css-in-js support for client components

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import styled from "styled-components";
+
+export default function Dashboard() {
+  return <StyledHeader>Dashboard</StyledHeader>;
+}
+
+const StyledHeader = styled.h1`
+  color: red;
+  background-color: blue;
+`;
---
+++ app/lib/registry.tsx
@@ -0,0 +1,33 @@
+"use client";
+/**
+ * Adds all styling to the top of the page to solve hydration problems on styles rendering
+ * Docs here: https://nextjs.org/docs/app/building-your-application/styling/css-in-js#styled-components
+ *  */
+
+import React, { useState } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+
+export default function StyledComponentsRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== "undefined") return <>{children}</>;
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      {children}
+    </StyleSheetManager>
+  );
+}
